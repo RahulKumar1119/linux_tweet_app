7c4db38e548efb6aed977b9fc20ad064e3de3014
Rahul Kumar Surati <suratirahulkumar29@gmail.com>
2022-01-21 13:06:56 +0000

    modified file

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 kubectl get deploy -n dev | grep pc-api | awk '{print $1}' > /home/ubuntu/list-deploy.txt
 
 while IFS= read -r line; do
-	kubectl set image deployment/$line -n dev pc-api=9870050478/linux_tweet_app:5.0
+	kubectl set image deployment/$line -n dev pc-api=9870050478/linux_tweet_app:{RELEASE_NAME}
 done < /home/ubuntu/list-deploy.txt
